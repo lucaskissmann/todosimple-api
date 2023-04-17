7e5e79897ea6301b3b015e57c46a31d77146cf4b
lucas kissmann <lucaskissmann@gmail.com>
2023-04-17 20:04:38 -0300

    Create Task model

--- src/main/java/com/kissmann/todosimple/models/User.java
+++ src/main/java/com/kissmann/todosimple/models/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -44,7 +45,16 @@ public class User
     @Size( groups = { CreateUser.class, UpdateUser.class}, min = 8, max =  60)
     private String password;
     
-    // private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany( mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
     
     public User(){
 
