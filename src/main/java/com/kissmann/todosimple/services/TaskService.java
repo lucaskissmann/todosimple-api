bcc48e9f618827890bfd5795bde5e70cfee68f38
lucas kissmann <lucaskissmann@gmail.com>
2023-04-21 12:17:29 -0300

    Refactor TaskService

--- src/main/java/com/kissmann/todosimple/services/TaskService.java
+++ src/main/java/com/kissmann/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.kissmann.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,11 @@ public class TaskService {
         Optional<Task> task = this.taskRepository.findById( id );
 
         return task.orElseThrow( () -> new RuntimeException(" Task not found for id " + id ));
+    }
+
+    public List<Task> getAllByUserId( Long userId ) {
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
     }
 
     @Transactional
