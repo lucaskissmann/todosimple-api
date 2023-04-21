14997a91b9586555e4e1ef6457b94764602bda2e
lucas kissmann <lucaskissmann@gmail.com>
2023-04-21 20:22:56 -0300

    Refactor Task Model

--- src/main/java/com/kissmann/todosimple/models/Task.java
+++ src/main/java/com/kissmann/todosimple/models/Task.java
@@ -46,11 +46,11 @@ public class Task
         this.description = description;
     }
 
-    public long getId() {
+    public Long getId() {
         return this.id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
