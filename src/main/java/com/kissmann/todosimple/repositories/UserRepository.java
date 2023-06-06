4def32d76eff68b7ba2d2ce19d0309cb9e8f6e55
Lucas Kissmann <lk@interact.com.br>
2023-06-06 17:21:35 -0300

    Create UserDetailsServiceImpl and JWTUtil

--- src/main/java/com/kissmann/todosimple/repositories/UserRepository.java
+++ src/main/java/com/kissmann/todosimple/repositories/UserRepository.java
@@ -2,10 +2,14 @@ package com.kissmann.todosimple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.kissmann.todosimple.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Transactional( readOnly = true )
+    User getByUsername( String username );
     
 }
