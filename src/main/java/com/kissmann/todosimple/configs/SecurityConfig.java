d67f7d62af9b35bc55b266ab9415be2858ed006f
Lucas Kissmann <lk@interact.com.br>
2023-06-20 13:46:01 -0300

    Create JWTAuthenticationFilter

--- src/main/java/com/kissmann/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/kissmann/todosimple/configs/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.kissmann.todosimple.security.JWTAuthenticationFilter;
 import com.kissmann.todosimple.security.JWTUtil;
 
 @Configuration
@@ -48,6 +49,8 @@ public class SecurityConfig {
         this.authenticationManager = authenticationManagerBuilder.build();
 
         http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated().and().authenticationManager(authenticationManager);
+
+        http.addFilter( new JWTAuthenticationFilter( this.authenticationManager, this.jwtUtil) );
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
